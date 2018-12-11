a5d79094e654a252c067be0144621a480dcaff33
BellineeeLai <laicy-wa16@student.tarc.edu.my>
2018-12-11 13:12:54 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/lab44networks/CourseAdapter.java
@@ -0,0 +1,44 @@
+package my.edu.tarc.lab44networks;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by TARC on 8/6/2015.
+ */
+public class CourseAdapter extends ArrayAdapter<Course> {
+
+    public CourseAdapter(Activity context, int resource, List<Course> list) {
+        super(context, resource, list);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Course course = getItem(position);//Retrieve a record
+
+        LayoutInflater inflater  = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.course_record, parent, false);
+
+        TextView textViewCode, textViewTitle, textViewCredit;
+
+        textViewCode = (TextView)rowView.findViewById(R.id.textViewCode);
+        textViewTitle = (TextView)rowView.findViewById(R.id.textViewTitle);
+        textViewCredit = (TextView)rowView.findViewById(R.id.textViewCredit);
+
+        textViewCode.setText(String.format("%s : %s",getContext().getString(R.string.code),course.getCode()));
+        textViewTitle.setText(String.format("%s : %s",getContext().getString(R.string.title),course.getTitle()));
+        textViewCredit.setText(String.format("%s : %s",getContext().getString(R.string.credit),course.getCredit()));
+        return rowView;
+    }
+}
